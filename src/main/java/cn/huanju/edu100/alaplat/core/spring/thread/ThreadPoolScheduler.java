9e5ee30b45a7f3ed807f5c8f458be2bc1931f920
SampeansXu <sampeans@163.com>
2023-10-09 23:25:38 +0800

    init

---
+++ src/main/java/cn/huanju/edu100/alaplat/core/spring/thread/ThreadPoolScheduler.java
@@ -0,0 +1,196 @@
+package cn.huanju.edu100.alaplat.core.spring.thread;
+
+import cn.huanju.edu100.alaplat.core.spring.thread.config.ThreadPoolProperties;
+import cn.huanju.edu100.alaplat.core.spring.thread.config.ThreadPoolSchedulerProperties;
+import cn.huanju.edu100.alaplat.core.spring.thread.pool.ThreadFuture;
+import cn.huanju.edu100.alaplat.core.spring.thread.pool.ThreadFutureTask;
+import cn.huanju.edu100.alaplat.core.spring.thread.pool.ThreadPoolInner;
+import cn.huanju.edu100.alaplat.core.spring.thread.pool.ThreadTask;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
+
+/**
+ * @Description: 线程池控制器
+ * @Author: xushengbin@hqwx.com
+ * @Date: 2021-12-13
+ */
+public class ThreadPoolScheduler extends ThreadPoolInner implements ApplicationContextAware {
+
+    private ScheduledExecutorService scheduledThreadPool = Executors.newSingleThreadScheduledExecutor();
+
+    private long checkTime;
+    private List<ThreadPoolProperties> threadPools;
+
+    public ThreadPoolScheduler(ThreadPoolSchedulerProperties config) {
+        Assert.isTrue(Objects.nonNull(config), "[hqconfig.threadpools-cheduler] is not config");
+        this.checkTime = config.getCheckTime();
+        this.threadPools = config.getThreadPools();
+    }
+
+    public ThreadPoolScheduler() {
+    }
+
+    public long getCheckTime() {
+        return checkTime;
+    }
+
+    public void setCheckTime(long checkTime) {
+        this.checkTime = checkTime;
+    }
+
+    public List<ThreadPoolProperties> getThreadPools() {
+        return threadPools;
+    }
+
+    public void setThreadPools(List<ThreadPoolProperties> threadPools) {
+        this.threadPools = threadPools;
+    }
+
+    @PostConstruct
+    private void create() {
+        ThreadPoolScheduler mainBean = this.applicationContext.getBean(ThreadPoolScheduler.class);
+        if (Objects.nonNull(mainBean)) {
+            mainBean.start();
+        } else {
+            this.start();
+        }
+    }
+
+    @PreDestroy
+    private void destroy() {
+        System.out.println("[ThreadPoolScheduler] scheduler destroy ready...");
+        if (Objects.nonNull(logger)) {
+            logger.info("[ThreadPoolScheduler] scheduler destroy ready...");
+        }
+
+        this.scheduledThreadPool.shutdown();
+        this.shutdown();
+    }
+
+    private void init() {
+        Assert.isTrue(this.checkTime > 0, "Config checkTime is invalid.");
+        Assert.isTrue(!CollectionUtils.isEmpty(this.threadPools), "Config threadPools is empty");
+        this.createPools(this.threadPools);
+    }
+
+    private void start() {
+        this.init();
+
+        this.scheduledThreadPool.scheduleAtFixedRate(() -> {
+            //定时任务
+            handleScheduled();
+        }, this.checkTime, this.checkTime, TimeUnit.SECONDS);
+
+        printDetail();
+        System.out.println("[ThreadPoolScheduler] scheduler started");
+        if (Objects.nonNull(logger)) {
+            logger.info("[ThreadPoolScheduler] scheduler started");
+        }
+    }
+
+    private void handleScheduled() {
+        this.printPoolsInfo();
+
+        List<String> allAlarms = this.checkAlarm();
+        if (!CollectionUtils.isEmpty(allAlarms)) {
+            for (String alarm : allAlarms) {
+                ReportAlarm.I().report(alarm);
+            }
+        }
+
+    }
+
+    private void printDetail() {
+        StringBuilder sb = new StringBuilder();
+        final String LineSeparator = "──────────────────────────────────────────────────────────────────────────────────" + System.lineSeparator();
+        sb.append(System.lineSeparator());
+        sb.append("┌" + LineSeparator);
+        sb.append("│ ThreadPoolScheduler Info: checkTime:" + this.checkTime + "s" + System.lineSeparator());
+        {
+            sb.append("├" + LineSeparator);
+            sb.append("│ ThreadPools Info:" + System.lineSeparator());
+            int idx = 1;
+            for (ThreadPoolProperties poolConfig : this.threadPools) {
+                sb.append("│ " + (idx++) + ".")
+                        .append("Name:" + poolConfig.getName())
+                        .append(", PoolSize:" + poolConfig.getPoolSize())
+                        .append(", AlarmTaskTime:" + poolConfig.getAlarmTaskTime() + "s")
+                        .append(", AlarmTaskCount:" + poolConfig.getAlarmTaskCount())
+                        .append(System.lineSeparator());
+            }
+        }
+        sb.append("└" + LineSeparator);
+
+        System.out.println(sb.toString());
+        if (Objects.nonNull(logger)) {
+            logger.info(sb.toString());
+        }
+    }
+
+    @Override
+    public void execute(String poolName, ThreadTask task) {
+        if (!StringUtils.hasText(poolName) || Objects.isNull(task)) {
+            throw new IllegalArgumentException("poolName or task is null");
+        }
+
+        this.addTask(poolName, task);
+    }
+
+    @Override
+    public <T> ThreadFuture<T> submit(String poolName, ThreadFutureTask<T> task) {
+        if (!StringUtils.hasText(poolName) || Objects.isNull(task)) {
+            throw new IllegalArgumentException("poolName or task is null");
+        }
+
+        this.addTask(poolName, task);
+        return task;
+    }
+
+    private ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+
+    /****************************************************************************************
+     * 报告异常
+     */
+    public static class ReportAlarm {
+        private static ReportAlarm inst = new ReportAlarm();
+
+        public static ReportAlarm I() {
+            return inst;
+        }
+
+        private ReportAlarm() {
+
+        }
+
+        private ExecutorService executors = Executors.newFixedThreadPool(2);
+
+        public void report(final String alarm) {
+            if (!StringUtils.hasText(alarm)) {
+                return;
+            }
+
+            executors.execute(() -> {
+                if (Objects.nonNull(logger)) {
+                    logger.error("[ThreadPoolScheduler] report alarm:{}", alarm);
+                }
+
+                throw new RuntimeException(alarm);
+            });
+        }
+    }
+}
